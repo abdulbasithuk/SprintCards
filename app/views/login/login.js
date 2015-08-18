60f943bbaa393f3836aac06843ff7aa3c24a0fa3
Abdul Basith Thajudeen <athajudeen@simplifydigital.co.uk>
2015-08-18 18:06:47 +0100

    Added custom directive for the login page

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,3 +1,19 @@
 /**
  * Created by athajudeen on 18/08/2015.
  */
+
+angular.module('login', [])
+
+    .directive('loginPage', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'views/login/login.html',
+            controller : ['$scope', function($scope){
+
+                $scope.submit = function(){
+                    console.log($scope.username, $scope.password);
+                };
+            }]
+        };
+    });
+
